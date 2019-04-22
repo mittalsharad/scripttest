204679ab2b59f8ab0bf482e7eeb17a48b4346c32
sharad mittal <mittal.sharad1994@gmail.com>
2019-04-22 13:28:06 +0530

    Commit Succesfull

---
+++ script.sh
@@ -0,0 +1,18 @@
+#GIT SCRIPT
+#1. Clone the repo
+#git clone ssh:git@github.com:sharad1994/scripttest.git
+
+#2. Authentication
+ssh -T git@github.com
+
+#3. check new files
+git status
+
+#4. add files
+git add .
+
+#5. commit
+git commit -m "Commit Succesfull"
+
+#6. PUSH
+git push
